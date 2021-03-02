b60afa3d38a661995feee3a139202ca29a1f4bdc
HaJunRyu <hjr4557@naver.com>
2021-03-02 15:05:18 +0900

    Feat: ES5 IIFE 모듈식 => ES6 ES Module로 코드 변경

--- src/js/modules/Euid/index.js
+++ src/js/modules/Euid/index.js
@@ -1,1 +1,6 @@
-/**@Euid 모듈 엔트리 파일 */
+/** @Euid 모듈 엔트리 파일 */
+import logger from './logger.js';
+import utils from './utils.js';
+import tester from './tester.js';
+
+export default { logger, utils, tester };
